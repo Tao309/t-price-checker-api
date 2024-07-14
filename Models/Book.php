104830d2a60c5ad9b140e422f1f2d21d95271956
Tao309 <taoman309@gmail.com>
2024-07-14 16:02:49 +0300

    Добавление отслеживание цены и комментарий в источник товара.

--- Models/Book.php
+++ Models/Book.php
@@ -19,19 +19,36 @@ use DateTime;
  *
  * @method BindingType getBindingType()
  *
- * @method setTitle(string $value)
- * @method setAuthor(string $value)
- * @method setIsbn(string $value)
- * @method setPages(int $value)
- * @method setCirculation(int $value)
- * @method setComment(string $value)
- * @method setSize(int $value)
- * @method setPublishYear(int $value)
+ * @method self setTitle(string $value)
+ * @method self setAuthor(string $value)
+ * @method self setIsbn(string $value)
+ * @method self setPages(int $value)
+ * @method self setCirculation(int $value)
+ * @method self setListenPriceValue(int $value)
+ * @method self setComment(string $value)
+ * @method self setSize(int $value)
+ * @method self setPublishYear(int $value)
  */
 class Book extends Entity
 {
     public const TABLE_PREFIX = 'b';
     public const TABLE_NAME = 'books';
+
+    public const PARAM_TITLE = 'title';
+    public const PARAM_AUTHOR = 'author';
+    public const PARAM_ISBN = 'isbn';
+    public const PARAM_PAGES = 'pages';
+    public const PARAM_CIRCULATION = 'circulation'; // тираж.
+    public const PARAM_SIZE = 'size'; // Размер.
+    public const PARAM_BINDING_TYPE_ID = 'binding_type_id';
+    public const PARAM_PUBLISH_YEAR = 'publish_year';
+    public const PARAM_RELEASE_DATE = 'release_date';
+    public const PARAM_LISTEN_PRICE_VALUE = 'listen_price_value';
+    public const PARAM_COMMENT = 'comment';
+    public const PARAM_DATE_UPDATED = 'date_updated';
+    public const PARAM_DATE_CREATED = 'date_created';
+
+    public const PARAM_BINDING_TYPE = 'binding_type';
 
     protected const PROPERTIES = [
         self::PARAM_ID => 'ID',
@@ -63,22 +80,6 @@ class Book extends Entity
             'relation_id' => Entity::PARAM_ID,
         ],
     ];
-
-    public const PARAM_TITLE = 'title';
-    public const PARAM_AUTHOR = 'author';
-    public const PARAM_ISBN = 'isbn';
-    public const PARAM_PAGES = 'pages';
-    public const PARAM_CIRCULATION = 'circulation'; // тираж.
-    public const PARAM_SIZE = 'size'; // Размер.
-    public const PARAM_BINDING_TYPE_ID = 'binding_type_id';
-    public const PARAM_PUBLISH_YEAR = 'publish_year';
-    public const PARAM_RELEASE_DATE = 'release_date';
-    public const PARAM_LISTEN_PRICE_VALUE = 'listen_price_value';
-    public const PARAM_COMMENT = 'comment';
-    public const PARAM_DATE_UPDATED = 'date_updated';
-    public const PARAM_DATE_CREATED = 'date_created';
-
-    public const PARAM_BINDING_TYPE = 'binding_type';
 
     protected string $title;
     protected ?string $author;
--- Models/SourceProduct.php
+++ Models/SourceProduct.php
@@ -6,26 +6,34 @@ use DateTime;
 
 /**
  * @method string getTitle()
+ * @method ?int getListenPriceValue()
+ * @method ?string getComment()
  * @method DateTime getDateCreated()
  *
  * @method SourceProductType getSourceProductType()
  *
- * @method setTitle(string $value)
+ * @method self setTitle(string $value)
+ * @method self setListenPriceValue(int $value)
+ * @method self setComment(string $value)
  */
 class SourceProduct extends Entity
 {
     public const TABLE_PREFIX = 'sp';
     public const TABLE_NAME = 'source_products';
 
-    public const PARAM_TITLE = 'title';
-    public const PARAM_DATE_CREATED = 'date_created';
     public const PARAM_SOURCE_PRODUCT_TYPE_ID = 'source_product_type_id';
+    public const PARAM_TITLE = 'title';
+    public const PARAM_LISTEN_PRICE_VALUE = 'listen_price_value';
+    public const PARAM_COMMENT = 'comment';
+    public const PARAM_DATE_CREATED = 'date_created';
 
     public const PARAM_SOURCE_PRODUCT_TYPE = 'source_product_type';
 
     protected const PROPERTIES = [
         self::PARAM_ID => 'ID',
         self::PARAM_TITLE => 'Название',
+        self::PARAM_LISTEN_PRICE_VALUE => 'Отслеживание цены',
+        self::PARAM_COMMENT => 'Комментарий',
         self::PARAM_DATE_CREATED => 'Дата создания',
     ];
 
@@ -43,6 +51,8 @@ class SourceProduct extends Entity
     ];
 
     protected string $title;
+    protected ?int $listenPriceValue;
+    protected ?string $comment;
     protected DateTime $dateCreated;
 
     protected SourceProductType $sourceProductType;
