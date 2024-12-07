b0d3c6ff477bac5d4a23e433d021bba141c4b452
Tao309 <taoman309@gmail.com>
2024-12-07 16:57:27 +0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$authToken = 'asfsdjdf893v34f3jk9w3rfnvjdgvjf893';
-
-//$mysqlHost = '92.53.96.136';
-$mysqlHost = 'vh400.timeweb.ru';
-$mysqlUser = $mysqlTable = 'fr51790_tprice';
-$mysqlPswrd = 'Jg73fjkew3fgd';
